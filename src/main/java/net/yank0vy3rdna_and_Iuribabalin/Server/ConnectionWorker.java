7cd4ce426574e0ac0e40c813c84bd7882a89e0a9
yank0vy3rdna <bibiwa2001@gmail.com>
2020-03-26 11:42:25 +0300

    Не отрубаю клиента

--- src/main/java/net/yank0vy3rdna_and_Iuribabalin/Server/ConnectionWorker.java
+++ src/main/java/net/yank0vy3rdna_and_Iuribabalin/Server/ConnectionWorker.java
@@ -58,58 +58,5 @@ public class ConnectionWorker {
 
         out.writeUTF(answ);
         out.flush();
-        in.close();
-        out.close();
-        socket.close();
     }
 }
-
-
-
-
-/*
-    public void processing(Socket socket) throws IOException {
-        DataInputStream in = new DataInputStream(socket.getInputStream());
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-        ByteBuffer buffer;
-
-        while (in.available()==0); // Ожидание данных
-
-
-        //Чтение размера строки с командой
-
-        byte[] bytes = new byte[4];
-        in.readFully(bytes,0,4);
-        buffer = ByteBuffer.wrap(bytes);
-        int size = buffer.getInt();
-
-
-        // Чтение строки с командой
-
-        bytes = new byte[size];
-        in.readFully(bytes, 0, size);
-        buffer = ByteBuffer.wrap(bytes);
-        String command = bb_to_str(buffer, StandardCharsets.UTF_8);
-
-
-        String answ;
-        System.out.println(in.available());
-        // Если есть, чтение доп данных
-        if(in.available()!=0){
-            bytes = new byte[in.available()];
-            in.readFully(bytes, 0, in.available());
-            buffer = ByteBuffer.wrap(bytes);
-            answ = dispatcher.dispatch(command, buffer);
-        }
-        else{
-            answ = dispatcher.dispatch(command);
-        }
-
-        // Отправка ответа
-
-        out.writeUTF(answ);
-        out.flush();
-        in.close();
-        out.close();
-        socket.close();
-    }*/
